48521674cd92dafa82e4c55c80523c7c20a91fab
klhansen4 <klhansen4@gmail.com>
2022-02-07 20:54:26 -0500

    created intake and shooter commands

---
+++ src/main/java/frc/robot/commands/ShooterYeetCommandPart2ElectricBoogaloo.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
+
+//yeet means to throw (cargo)
+//electric boogaloo: idk it's a meme ig
+
+public class ShooterYeetCommandPart2ElectricBoogaloo extends CommandBase{
+    private ShooterSubsystem m_shooterSubsystem;
+    private double m_shooterYeetSpeed;
+}
